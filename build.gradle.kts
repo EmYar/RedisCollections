9c7123de08fbfc4f08a7d968b82a17a3f297e1b0
Yaroslav Emelyanov <emel.yar95@gmail.com>
2024-10-26 18:37:43 +0400

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,13 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(libs.jedis)
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation(libs.testContainers)
+    testImplementation(libs.testContainersRedis)
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
