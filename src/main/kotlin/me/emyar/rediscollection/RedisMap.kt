3a91518d8394477e014ca65fad7a3e0cbf903b76
Yaroslav Emelyanov <emel.yar95@gmail.com>
2024-10-27 01:33:30 +0400

    Basic implementation of Map

--- src/main/kotlin/me/emyar/rediscollection/RedisMap.kt
+++ src/main/kotlin/me/emyar/rediscollection/RedisMap.kt
@@ -3,25 +3,25 @@ package me.emyar.rediscollection
 import redis.clients.jedis.Jedis
 import java.util.function.BiConsumer
 import java.util.function.BiFunction
-import java.util.function.Function
 
-private const val BATCH_SIZE = 1000
+private const val CHUNK_SIZE = 1000
 
-class StringRedisMap(private val jedis: Jedis) : MutableMap<String, String> {
+class StringRedisMap(private val jedis: Jedis) : MutableMap<String, Int> {
 
-    override val entries: MutableSet<MutableMap.MutableEntry<String, String>>
+    override val entries: MutableSet<MutableMap.MutableEntry<String, Int>>
         get() = keys.asSequence()
-            .zip(values.asSequence(), ::Entry)
+            .zip(values.asSequence()) { key, value -> Entry(key, value, jedis) }
             .toMutableSet()
 
     override val keys: MutableSet<String>
         get() = jedis.keys("*")
 
     override val size: Int
-        get() = jedis.dbSize().toInt() // TODO
+        get() = jedis.dbSize().toInt()
 
-    override val values: MutableCollection<String>
+    override val values: MutableCollection<Int>
         get() = jedis.mget(*jedis.keys("*").toTypedArray())
+            .mapTo(mutableListOf(), String::toInt)
 
     override fun clear() {
         jedis.flushDB()
@@ -30,103 +30,107 @@ class StringRedisMap(private val jedis: Jedis) : MutableMap<String, String> {
     override fun isEmpty(): Boolean =
         size == 0
 
-    override fun remove(key: String): String? =
+    override fun remove(key: String): Int? =
         jedis.getDel(key)
             .nilAsNull()
+            ?.toInt()
 
-    override fun putAll(from: Map<out String, String>) {
-        from.forEach { (k, v) -> jedis.set(k, v) }
+    override fun putAll(from: Map<out String, Int>) {
+        val keysvalues = ArrayList<String>(from.size * 2)
+        from.forEach {
+            keysvalues += it.key
+            keysvalues += it.value.toString()
+        }
+        jedis.mset(*keysvalues.toTypedArray())
     }
 
-    override fun put(key: String, value: String): String? =
-        jedis.getSet(key, value)
+    override fun put(key: String, value: Int): Int? =
+        jedis.getSet(key, value.toString())
             .nilAsNull()
+            ?.toInt()
 
-    override fun get(key: String): String? =
+    override fun get(key: String): Int? =
         jedis.get(key)
             .nilAsNull()
+            ?.toInt()
 
-    override fun containsValue(value: String): Boolean =
-        keys.asSequence()
-            .chunked(BATCH_SIZE)
-            .any { keys ->
-                jedis.mget(*keys.toTypedArray()).contains(value)
+    override fun containsValue(value: Int): Boolean {
+        val stringValue = value.toString()
+        return keys.asSequence()
+            .chunked(CHUNK_SIZE)
+            .any { keysChunk ->
+                jedis.mget(*keysChunk.toTypedArray())
+                    .contains(stringValue)
             }
+    }
 
     override fun containsKey(key: String): Boolean =
         jedis.exists(key)
 
-    override fun remove(key: String, value: String): Boolean =
-        jedis.get(key) == value &&
+    override fun remove(key: String, value: Int): Boolean =
+        jedis.get(key).toInt() == value &&
                 jedis.del(key) == 1L
 
-    override fun compute(key: String, remappingFunction: BiFunction<in String, in String?, out String?>): String? {
-        return super.compute(key, remappingFunction)
+    override fun replace(key: String, oldValue: Int, newValue: Int): Boolean =
+        if (jedis.get(key).toInt() == oldValue) {
+            jedis.set(key, newValue.toString())
+            true
+        } else {
+            false
+        }
+
+    override fun replace(key: String, value: Int): Int? =
+        if (jedis.exists(key)) {
+            jedis.getSet(key, value.toString()).toInt()
+        } else {
+            null
+        }
+
+    override fun replaceAll(function: BiFunction<in String, in Int, out Int>) {
+        keys.asSequence()
+            .chunked(CHUNK_SIZE)
+            .forEach { keysChunk ->
+                val values = jedis.mget(*keysChunk.toTypedArray())
+                    .map(String::toInt)
+                val keysvalues = ArrayList<String>(keysChunk.size * 2)
+                keysChunk.forEachIndexed { i, key ->
+                    keysvalues += key
+                    keysvalues += function.apply(key, values[i]).toString()
+                }
+                jedis.mset(*keysvalues.toTypedArray())
+            }
     }
 
-    override fun computeIfAbsent(key: String, mappingFunction: Function<in String, out String>): String {
-        return super.computeIfAbsent(key, mappingFunction)
-    }
-
-    override fun computeIfPresent(
-        key: String,
-        remappingFunction: BiFunction<in String, in String, out String?>
-    ): String? {
-        return super.computeIfPresent(key, remappingFunction)
-    }
-
-    override fun merge(
-        key: String,
-        value: String,
-        remappingFunction: BiFunction<in String, in String, out String?>
-    ): String? {
-        return super.merge(key, value, remappingFunction)
-    }
-
-    override fun putIfAbsent(key: String, value: String): String? {
-        return super.putIfAbsent(key, value)
-    }
-
-    override fun replace(key: String, oldValue: String, newValue: String): Boolean {
-        return super.replace(key, oldValue, newValue)
-    }
-
-    override fun replace(key: String, value: String): String? {
-        return super.replace(key, value)
-    }
-
-    override fun replaceAll(function: BiFunction<in String, in String, out String>) {
-        super.replaceAll(function)
-    }
-
-    override fun getOrDefault(key: String, defaultValue: String): String =
+    override fun getOrDefault(key: String, defaultValue: Int): Int =
         get(key) ?: defaultValue
 
-    //TODO - выяснить, может ли храниться ключ без значения
-    override fun forEach(action: BiConsumer<in String, in String>) {
+    override fun forEach(action: BiConsumer<in String, in Int>) {
         keys.asSequence()
-            .chunked(BATCH_SIZE)
-            .flatMap { keys ->
-                val values = jedis.mget(*keys.toTypedArray())
-                keys.zip(values) { key, value -> value.nilAsNull()?.let { key to it } }
+            .chunked(CHUNK_SIZE)
+            .forEach { keysChunk ->
+                val values = jedis.mget(*keysChunk.toTypedArray())
+                    .map(String::toInt)
+                keysChunk.forEachIndexed { i, key ->
+                    action.accept(key, values[i])
+                }
             }
-            .filterNotNull()
-            .forEach { (key, value) -> action.accept(key, value) }
     }
 }
 
 private class Entry(
     override val key: String,
-    value: String
-) : MutableMap.MutableEntry<String, String> {
+    value: Int,
+    private val jedis: Jedis,
+) : MutableMap.MutableEntry<String, Int> {
     override var value = value
         private set
 
-    override fun setValue(newValue: String): String {
+    override fun setValue(newValue: Int): Int {
         val oldValue = value
+        jedis.set(key, newValue.toString())
         value = newValue
         return oldValue
     }
 }
 
-private fun String.nilAsNull() = takeIf { it != "nil" }
+private fun String.nilAsNull(): String? = takeIf { it != "nil" }
