50734e13cb49eca828114a529d672e72a1e43187
Yaroslav Emelyanov <emel.yar95@gmail.com>
2025-01-10 17:45:45 +0400

    Upgrades Redis version in tests

--- src/test/kotlin/me/emyar/rediscollection/RedisContainer.kt
+++ src/test/kotlin/me/emyar/rediscollection/RedisContainer.kt
@@ -3,5 +3,5 @@ package me.emyar.rediscollection
 import com.redis.testcontainers.RedisContainer
 import org.testcontainers.utility.DockerImageName
 
-val redisContainer = RedisContainer(DockerImageName.parse("redis:6.2.6"))
+val redisContainer = RedisContainer(DockerImageName.parse("redis:7.4.2"))
     .apply { start() }
